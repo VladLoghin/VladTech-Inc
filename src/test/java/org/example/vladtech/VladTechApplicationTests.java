935f233ea7ba78ce5f01b34bfb7d92b827229fe7
piplupcool123 <christianvehicle@gmail.com>
2025-12-02 14:39:44 -0500

    hopefully fixes github if not ill revert

--- src/test/java/org/example/vladtech/VladTechApplicationTests.java
+++ src/test/java/org/example/vladtech/VladTechApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.vladtech;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class VladTechApplicationTests {
 
 	@Test
