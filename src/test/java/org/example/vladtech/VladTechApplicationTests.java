fdeef2ecdeef85bbd5a85b371a226ca4a0e297cc
piplupcool123 <christianvehicle@gmail.com>
2025-12-02 14:49:17 -0500

    removed unnecessary

--- src/test/java/org/example/vladtech/VladTechApplicationTests.java
+++ src/test/java/org/example/vladtech/VladTechApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 @Disabled("Conflicts with OAuth2/MongoDB config in CI - context is validated by other integration tests")
 class VladTechApplicationTests {
 
