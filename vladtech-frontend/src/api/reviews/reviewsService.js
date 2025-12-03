5533c499d8a286218114156f8b2d3bfda5edb291
Jack <jack.touchpad@gmail.com>
2025-12-03 17:26:58 -0500

    removed v1 from endpoint and fixed some TS errors

--- vladtech-frontend/src/api/reviews/reviewsService.js
+++ vladtech-frontend/src/api/reviews/reviewsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8080/api/v1/reviews";
+const API_BASE = "http://localhost:8080/api/reviews";
 
 export const getAllVisibleReviews = async () => {
     try {
