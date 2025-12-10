1e8cd9227a7bec38403e6f0afd112b9e60cc2bb0
Drago935 <2233184@champlaincollege.qc.ca>
2025-12-10 12:58:18 -0500

    fixed logging in -> redirect to home page... now they should stay where they were
    # Conflicts:
    #	.idea/workspace.xml
    #	vladtech-frontend/src/App.jsx

--- vladtech-frontend/src/auth/Auth0ProviderWithConfig.jsx
+++ vladtech-frontend/src/auth/Auth0ProviderWithConfig.jsx
@@ -1,9 +1,13 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const Auth0ProviderWithConfig = ({ children }) => {
+const Auth0ProviderWithConfig = ({ children, navigate }) => {
   const domain = "dev-ljz84r2xvrlnftfv.ca.auth0.com";
   const clientId = "sDVdjRgneqMMYuQm8njufqcG0yrPV2j6";
   const audience = "https://vladtech/api";
+
+  const onRedirectCallback = (appState) => {
+      navigate(appState?.returnTo || window.location.pathname);
+  };
 
   return (
     <Auth0Provider
@@ -14,6 +18,7 @@ const Auth0ProviderWithConfig = ({ children }) => {
         audience,
         scope: "openid profile email"
       }}
+      onRedirectCallback={onRedirectCallback}
     >
       {children}
     </Auth0Provider>
