785d1462308a36d801f446a693479f6209071330
piplupcool123 <christianvehicle@gmail.com>
2025-12-14 22:01:04 -0500

    Update ReviewControllerIntegrationTest.java

--- vladtech-backend/src/test/java/org/example/vladtech/reviews/presentation/ReviewControllerIntegrationTest.java
+++ vladtech-backend/src/test/java/org/example/vladtech/reviews/presentation/ReviewControllerIntegrationTest.java
@@ -31,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("ci")
 class ReviewControllerIntegrationTest {
 
     @Autowired
