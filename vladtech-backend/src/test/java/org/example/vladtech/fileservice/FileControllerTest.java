d8ed4b10db4b9494516be0055d14ef34adc6643f
Jack <jack.touchpad@gmail.com>
2026-01-03 00:14:08 -0500

    should fix tests failing

--- vladtech-backend/src/test/java/org/example/vladtech/fileservice/FileControllerTest.java
+++ vladtech-backend/src/test/java/org/example/vladtech/fileservice/FileControllerTest.java
@@ -30,7 +30,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 class FileControllerTest {
 
     @Autowired
