17e2cc014431f337b062324cd40c3c02a9091f1a
piplupcool123 <christianvehicle@gmail.com>
2026-02-04 17:56:44 -0500

    toggle

--- vladtech-backend/src/main/java/org/example/vladtech/projectsubdomain/presentationlayer/ProjectCalendarEntryResponseModel.java
+++ vladtech-backend/src/main/java/org/example/vladtech/projectsubdomain/presentationlayer/ProjectCalendarEntryResponseModel.java
@@ -17,5 +17,6 @@ public class ProjectCalendarEntryResponseModel {
     private LocalDate startDate;
     private LocalDate dueDate;
     private String status;
+    private String state;
     private String projectType;
 }
